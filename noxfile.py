b66bdcfb0b8864a20664edb209476591b5802610
June <176018493+junapur@users.noreply.github.com>
2025-05-20 21:47:44 -0400

    chore: speed up ci

--- noxfile.py
+++ noxfile.py
@@ -4,42 +4,21 @@ nox.options.default_venv_backend = "uv"
 nox.options.sessions = ["lint", "type_check"]
 
 
-@nox.session
+@nox.session(venv_backend="none")
 def lint(session: nox.Session) -> None:
     """Check for lint issues and proper formatting."""
-    session.run_install(
-        "uv",
-        "sync",
-        "--locked",
-        "--quiet",
-        env={"UV_PROJECT_ENVIRONMENT": session.virtualenv.location},
-    )
     session.run("ruff", "check", "--no-fix")
     session.run("ruff", "format", "--check")
 
 
-@nox.session
+@nox.session(venv_backend="none")
 def tidy(session: nox.Session) -> None:
     """Fix any fixable lint issues and format the code."""
-    session.run_install(
-        "uv",
-        "sync",
-        "--locked",
-        "--quiet",
-        env={"UV_PROJECT_ENVIRONMENT": session.virtualenv.location},
-    )
     session.run("ruff", "check", "--fix")
     session.run("ruff", "format")
 
 
-@nox.session
+@nox.session(venv_backend="none")
 def type_check(session: nox.Session) -> None:
     """Validate type correctness."""
-    session.run_install(
-        "uv",
-        "sync",
-        "--locked",
-        "--quiet",
-        env={"UV_PROJECT_ENVIRONMENT": session.virtualenv.location},
-    )
     session.run("pyright")
